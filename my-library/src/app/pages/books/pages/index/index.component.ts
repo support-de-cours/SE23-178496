10a58ee31df746478fdb5b4a65fc0a57a2ae8227
Arnaud BODEL <contact@osw3.net>
2023-07-04 16:57:24 +0200

    -

--- my-library/src/app/pages/books/pages/index/index.component.ts
+++ my-library/src/app/pages/books/pages/index/index.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss']
 })
-export class IndexComponent {
+export class IndexComponent implements OnInit
+{
+  public books: any[] = [];
 
+
+
+  ngOnInit(): void 
+  {
+    setTimeout(() => {
+      this.books = [
+        "Book 1",
+        "Book 2",
+        "Book 3",
+        "Book 4",
+      ]
+    },10000);
+  }
 }
